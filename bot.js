875388c319d2dbe8eb6cc90d6bdecd81e0772a75
madmad414 <72622362+madmad414@users.noreply.github.com>
2020-10-10 04:08:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(``,"")
-client.user.setStatus("online")
+client.user.setStatus("offline")
 });
 
 
